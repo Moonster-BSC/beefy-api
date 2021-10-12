9b7effa577766462cc5eed14e13169fa71b62174
Moonster <moonster.bsc@gmail.com>
2021-10-12 09:31:36 -0400

    it was just that easy. This serves the UI at docs

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import 'reflect-metadata';
-import { createKoaServer, getMetadataArgsStorage, useKoaServer } from 'routing-controllers';
+import { getMetadataArgsStorage, useKoaServer } from 'routing-controllers';
 import { routingControllersToSpec } from 'routing-controllers-openapi';
 import { BeefyController } from './beefyController';
+import * as swaggerUiExpress from 'swagger-ui-express';
 
 const routingControllersOptions = {
   controllers: [BeefyController],
@@ -16,6 +17,7 @@ import etag from 'koa-etag';
 
 import rt from './middleware/rt';
 import powered from './middleware/powered';
+import { koaSwagger } from 'koa2-swagger-ui';
 // import router from './router';
 
 const app = new Koa();
@@ -32,6 +34,24 @@ app.context.cache = {};
 
 // app.use(router.routes());
 // app.use(router.allowedMethods());
+
+// Parse routing-controllers classes into OpenAPI spec:
+const storage = getMetadataArgsStorage();
+const spec = routingControllersToSpec(storage, routingControllersOptions, {
+  info: {
+    description: 'Generated with `routing-controllers-openapi`',
+    title: 'A sample API',
+    version: '1.0.0',
+  },
+});
+
+app.use(
+  koaSwagger({
+    swaggerOptions: {
+      spec,
+    },
+  })
+);
 
 useKoaServer(app, routingControllersOptions);
 
